0ffec79c6dc599d1e8335b3af42231cab34d1ce8
masyune4ka <gleb.khashagulgov@inbox.ru>
2026-06-28 16:30:04 +0600

    added implementation of StringExtensions with tests

---
+++ practice2026/task01/StringExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace task01
+{
+    public static class StringExtensions
+    {
+        public static bool IsPalindrome(this string input)
+        {
+            if (input == null || input.Length == 0)
+            {
+                return false;
+            }
+            string lowerInput = input.ToLower();
+
+            string cleaned = "";
+            foreach (char c in lowerInput)
+            {
+                if (!char.IsPunctuation(c) && !char.IsWhiteSpace(c))
+                {
+                    cleaned += c;
+                }
+            }
+            if (cleaned == "")
+            {
+                return false;
+            }
+            for (int i = 0; i < cleaned.Length / 2; i++)
+            {
+                if (cleaned[i] != cleaned[cleaned.Length - 1 - i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
+
